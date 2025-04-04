7017ed9ceccde3e2719f07aa82f74c08424dc204
Joynal Abedin <joynal8662@gmail.com>
2025-04-05 04:31:45 +0600

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,33 @@
+city = ['dhaka','khulna','barishal','dhaka']
+print(city)
+
+city.append('bogura')
+print(city)
+
+city.insert(2,'magura')
+print(city)
+
+
+a=city.index('magura')
+print(a)
+
+city.pop()
+print(city)
+
+city.remove('dhaka')
+print(city)
+
+c= city.count('dhaka')
+print(c)
+
+city.sort()
+print(city)
+
+city.reverse()
+print(city)
+
+city.clear()
+print(city)
+
+
+#this is all about of list methods.
